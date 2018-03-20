3a30f679ae278d1adf73937d5e209460fff13a56
Alex McKean <amckean@me.com>
2018-03-20 18:44:08 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,27 +7,31 @@ require 'active_support/inflector'
       self.to_s.downcase.pluralize
     end
 
-      def self.column_names
-        DB[:conn].results_as_hash = true
+    def self.column_names
+      DB[:conn].results_as_hash = true
 
-        sql = "PRAGMA table_info('#{table_name}')"
+      sql = "PRAGMA table_info('#{table_name}')"
 
-        table_info = DB[:conn].execute(sql)
-        column_names = []
+      table_info = DB[:conn].execute(sql)
+      column_names = []
 
-        table_info.each do |column|
-          column_names << column["name"]
-        end
+      table_info.each do |column|
+        column_names << column["name"]
+      end
         column_names.compact
-      end
+    end
 
-      self.column_names.each do |col_name|
-        attr_accessor col_name.to_sym
+    def initialize(options={})
+      options.each do |property, value|
+        self.send("#{property}=", value)
       end
+    end
 
-      def initialize(options={})
-        options.each do |property, value|
-          self.send("#{property}=", value)
-        end
-      end
+    def table_name_for_insert
+      self.class.table_name
+    end
+
+    def save
+
+    end
 end
