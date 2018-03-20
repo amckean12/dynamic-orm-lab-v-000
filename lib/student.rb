6b07c53eaf66dda30673e806d32200d9084e36fb
Alex McKean <amckean@me.com>
2018-03-20 18:37:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,10 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  
+    self.column_names.each do |col_name|
+      attr_accessor col_name.to_sym
+    end
 
   def initialize(options={})
     options.each do |property, value|
@@ -28,7 +32,4 @@ class Student < InteractiveRecord
     column_names.compact
   end
 
-    self.column_names.each do |col_name|
-      attr_accessor col_name.to_sym
-    end
 end
